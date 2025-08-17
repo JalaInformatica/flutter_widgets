86985b4b9248c28670e88294e51373824ff9006c
Muhammad Akhsanul Zidan <92286610+MuhammadAkhsanulZidan@users.noreply.github.com>
2025-08-17 20:37:26 +0700

    update

--- lib/flutter_widgets.dart
+++ lib/flutter_widgets.dart
@@ -1,27 +1,5 @@
 library flutter_widgets;
 
-// Export all your widgets
-export 'src/app_cached_image.dart';
-export 'src/app_checkbox.dart';
-export 'src/app_clickable_text.dart';
-export 'src/app_confirmation_dialog.dart';
-export 'src/app_dialog.dart';
-export 'src/app_elevated_button.dart';
-export 'src/app_favorite_button.dart';
-export 'src/app_icon_button.dart';
-export 'src/app_icon.dart';
-export 'src/app_image.dart';
-export 'src/app_loading_button.dart';
-export 'src/app_normal_button.dart';
-export 'src/app_product_card.dart';
-export 'src/app_refresh_indicator.dart';
-export 'src/app_shimmer.dart';
-export 'src/app_table3.dart';
-export 'src/app_text_field.dart';
-export 'src/app_text_form_field.dart';
-export 'src/app_tile.dart';
-export 'src/app_dropdown.dart';
-
 // Export theming helpers
 export 'theme/app_color.dart';
 export 'theme/app_text_style.dart';
